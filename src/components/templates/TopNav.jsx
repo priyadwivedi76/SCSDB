a433c13e42df7e3971d27a00244ee257c8f27b64
Dwivedi Priya <pd7668665@gmail.com>
2024-11-22 18:30:01 +0530

    Navigation/Search

--- src/components/templates/TopNav.jsx
+++ src/components/templates/TopNav.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "../../utils/axios";
 import { Link } from "react-router-dom";
+import noImage from "/noImage.jpg";
 
 const TopNav=() => {
     const [query,setquery]=useState("");
-    console.log(query)
+    const [searches,setSearches]=useState([]);
+
+    const getSearches=async()=>{
+        try {
+            const {data}=await axios.get(`/search/multi?query=${query}`);
+            setSearches(data.results);
+        } catch (error) {
+            console.log("Error: " + error)
+        }
+    }
+
+    useEffect(()=>{
+        getSearches();
+    },[query])
+
     return(
         <>
         <div className="w-full h-[10vh] relative flex justify-start items-center ml-[15%]">
-            <i class="text-3xl text-zinc-400 ri-search-line"></i>
+            <i className="text-3xl text-zinc-400 ri-search-line"></i>
             {/* Target input and print them as query */}
             <input 
             type="text" 
@@ -19,13 +35,17 @@ const TopNav=() => {
             {query.length > 0 && (
                 <i 
                 onClick={()=>setquery("")}
-                class="text-3xl text-zinc-400 ri-close-large-line"></i>
+                className="text-3xl text-zinc-400 ri-close-large-line"></i>
             )}
             <div className="absolute max-h-[50vh] w-[50%] bg-zinc-200 top-[90%] overflow-auto rounded">
-                {/* <Link className="hover:text-black duration-300 hover:bg-zinc-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100">
-                    <img src="" alt="" />
-                    <span>Hello</span> 
-                </Link> */}
+
+                {searches.map((search,index)=>(
+                    <Link key={index} className="hover:text-black duration-300 hover:bg-zinc-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100">
+
+                    <img src={search.backdrop_path || search.profile_path ? `https://image.tmdb.org/t/p/original/${search.backdrop_path || search.profile_path}` : noImage} className="h-[10vh] w-[10vh] object-cover rounded mr-5" alt="" />
+                    <span>{search.name || search.title || search.original_name || search.original_title}</span> 
+                    </Link>
+                ))}
             </div>
         </div>
         </>
--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+
 import axios from "axios";
 
-const instance=()=>{
-    baseURL:"https://api.themoviedb.org/3/"
-}
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzM0NTRjYWU0ZjkxYjJkMzJkYzFiNTcxNjkzMDg4YyIsIm5iZiI6MTczMjI2Nzg0OC45OTY3NjgsInN1YiI6IjY3M2M4YjUxNjBiN2IzYmM5NGEwYmViNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.auQCPuNXfW7Bn0uFQi25xMkCbtaUFkPasSyOL29bT-8'
+    }
+});
+
+export default instance;
