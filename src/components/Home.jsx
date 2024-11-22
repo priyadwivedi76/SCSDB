8a74a45853f2f50a0bd3de74d2511914c0e0d4ff
Dwivedi Priya <pd7668665@gmail.com>
2024-11-22 20:59:34 +0530

    Header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SideNav from "./templates/SideNav";
 import TopNav from "./templates/TopNav";
+import Header from "./templates/Header";
+import axios from "../utils/axios";
 
 const Home=()=>{
     document.title="SCSDB | Home";
-    return(
+    const [wallpaper,setWallpaper]=useState(null)
+
+
+    const GetHeaderWallpaper=async()=>{
+        try {
+            const {data}=await axios.get(`/trending/all/day`);
+            const randomData=data.results[(Math.random()*data.results.length).toFixed()];
+            setWallpaper(randomData);
+        } catch (error) {
+            console.log("Error: " + error)
+        }
+    }
+
+    useEffect(()=>{
+        !wallpaper && GetHeaderWallpaper();
+    },[])
+
+
+    return wallpaper ? (
         <>
             <SideNav/>
             <div className="w-[80%] h-full">
                 <TopNav/>
+                <Header data={wallpaper}/>
             </div>
         </>
-    )
+    ):<h1>Loading</h1>
 }
 
 export default Home;
--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header=()=>{
+const Header=({data})=>{
     return(
         <>
+        <div 
+        style={{
+            background:`linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.5),rgba(0,0,0,0.7)),url(https://image.tmdb.org/t/p/original/${data.backdrop_path || data.profile_path})`,
+            backgroundPosition:`center`,
+            backgroundSize:`cover`
+        }}
+        className="w-full h-[60vh] flex flex-col justify-end items-start p-[5%]">
+            <h1 className="w-[70%] mb-3 text-5xl text-white font-black">{data.name || data.title || data.original_name || data.original_title}</h1>
+            <p className="w-[70%] text-white">{data.overview.slice(0,200)}...<Link className="text-blue-300 font-semibold">more</Link></p>
+            <p className="text-white mt-2">
+                <i className="text-yellow-500 ri-megaphone-fill"></i>{" "}{data.release_date || "No Information"}
+                <i className="ml-5 text-yellow-500 ri-film-fill"></i>{" "}{data.media_type.toUpperCase()}
+            </p>
+            <Link className="mt-4 bg-[#6556CD] p-3 rounded text-white">Watch Trailer</Link>
+        </div>
         </>
     )
 }
